3d3ff5e62df6f93329ace178af1a4eefc1b69b88
Andrey Sholinka <sholinka2111@gmail.com>
2020-12-12 11:34:27 +0700

    menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ const connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Application REST API is running", res)
 };
+
+//!menampilam semua data mahasiswa
+exports.tampilSemuaMahasiswa = (req, res) => {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+        if (error) {
+            connection.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
