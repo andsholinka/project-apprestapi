3ec35787268b4fe179900cd100f713a74d0bea00
Andrey Sholinka <sholinka2111@gmail.com>
2020-12-15 22:49:50 +0700

    update authorization

--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -2,36 +2,33 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/secret');
 
 function verifikasi(role) {
-    return function (req, res) {
+    return function (req, res, next) {
         //cek auth header
-        var tokenWithBearer = req.headers.authorization;
-        if (tokenWithBearer) {
-            var token = tokenWithBearer.split(' ')[1];
-            //verifikasi
-            jwt.verify(token, config.secret, function (err, decoded) {
-                if (err) {
+        var token = req.headers['authorization'];
+        if (!token) return res.status(401).send({
+            auth: false,
+            message: 'No token provided.'
+        });
+        //verifikasi
+        jwt.verify(token, config.secret, function (err, decoded) {
+            if (err) {
+                return res.status(401).send({
+                    auth: false,
+                    message: 'Token tidak terdaftar!'
+                });
+            } else {
+                // console.log(role)
+                if (role == 1) {
+                    req.auth = decoded;
+                    next();
+                } else {
                     return res.status(401).send({
                         auth: false,
-                        message: 'Token tidak terdaftar!'
+                        message: 'Gagal mengotorisasi role anda!'
                     });
-                } else {
-                    if (role == 2) {
-                        req.auth = decoded;
-                        next();
-                    } else {
-                        return res.status(401).send({
-                            auth: false,
-                            message: 'Gagal mengotorisasi role anda!'
-                        });
-                    }
                 }
-            });
-        } else {
-            return res.status(401).send({
-                auth: false,
-                message: 'Token tidak tersedia!'
-            });
-        }
+            }
+        });
     }
 }
 
