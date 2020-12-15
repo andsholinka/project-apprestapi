033718dec7c3c06cac0a8f14b93c6012d1795f33
Andrey Sholinka <sholinka2111@gmail.com>
2020-12-15 22:50:11 +0700

    update verifikasi

--- routes.js
+++ routes.js
@@ -1,20 +1,28 @@
 'use strict';
 
+const verifikasi = require('./middleware/verifikasi');
+
 module.exports = function (app) {
     var jsonku = require('./controller');
 
     app.route('/')
         .get(jsonku.index);
+
     app.route('/tampil')
         .get(jsonku.tampilSemuaMahasiswa);
+
     app.route('/tampil/:id')
         .get(jsonku.tampilBerdasarkanId);
+
     app.route('/tambah')
-        .post(jsonku.tambahMahasiswa);
+        .post(verifikasi(1), jsonku.tambahMahasiswa);
+
     app.route('/ubah/:id')
-        .put(jsonku.updateMahasiswa);
+        .put(verifikasi(1), jsonku.updateMahasiswa);
+
     app.route('/hapus/:id')
-        .delete(jsonku.hapusMahasiswa);
+        .delete(verifikasi(1), jsonku.hapusMahasiswa);
+
     app.route('/tampilmatakuliah')
-        .get(jsonku.tampilGroupMatakuliah);
+        .get(verifikasi(1), jsonku.tampilGroupMatakuliah);
 }
