cab4bdb099ee27ad86cd8f803eb68c743cd8b4a8
Andrey Sholinka <sholinka2111@gmail.com>
2020-12-15 07:35:16 +0700

    dotenv

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const auth = require('./auth');
+const verifikasi = require('./verifikasi');
 const router = express.Router();
 
-//!daftarkan menu registrasi
+//! daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
 
-//!daftarkan menu login
+//! daftarkan menu login
 router.post('/api/v1/login', auth.login);
+
+router.post('/api/v1/ubahpassword', verifikasi(1), auth.ubahPassword);
+
+router.get('/verify', auth.verifikasi)
+
+//! halaman menampilkan data tabel oleh administrator
+router.get('/api/v1/admin/mahasiswa', verifikasi(1), auth.adminmahasiswa);
 
 module.exports = router;
