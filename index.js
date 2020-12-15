01d1ae44c746d1cfa0084116d7fd7db530a97a42
Andrey Sholinka <sholinka2111@gmail.com>
2020-12-15 07:35:44 +0700

    dotenv

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -16,8 +18,8 @@ routes(app);
 //daftarkan menu routes dari index
 app.use('/auth', require('./middleware'));
 
-app.listen(8000, () => {
-    console.log('Example app listening on port 8000!');
+app.listen(process.env.PORT, () => {
+    console.log(`App listens to port ${process.env.PORT}`);
 });
 
 //Run app, then load http://localhost:port in a browser to see the output.
